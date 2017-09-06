336e695e6be801858993fd989e04e9345788737e
Minh Nguyen <kuroemon95@gmail.com>
2017-09-06 18:06:30 -0500

    add mini proxy server

---
+++ src/Booking.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Booking extents React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      
+    }
+  }
+  render() {
+    return() {
+
+    }
+  }
+}
